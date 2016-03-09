73127111f4effd7df7bd340dc38ca162447e85ba
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-08 22:59:13 -0500

    Update the delegator to better pretend

--- libraries/common-delegator-obfuscated_type.rb
+++ libraries/common-delegator-obfuscated_type.rb
@@ -5,8 +5,16 @@ module Common
     class ObfuscatedType < SimpleDelegator
       include Common::Mixin::ObfuscatedType
 
-      def initialize(value)
-        super(value)
+			def class
+			  __getobj__.class
+      end
+
+      def is_a?(*args)
+        __getobj__.is_a?(*args)
+      end
+
+      def kind_of?(*args)
+        __getobj__.kind_of?(*args)
       end
     end
   end
