682df6f5c254a2df8992b3e97ed33072d6521b24
Jonathan Serafini <jonathan@lightspeedpos.com>
2016-03-12 09:41:50 -0500

    Update attributes documentation

--- attributes/common_attrs-blacklist.rb
+++ attributes/common_attrs-blacklist.rb
@@ -1,4 +1,7 @@
 
 # Hash of attributes to blacklist
+# - hash key will be a period separated path to the attribute to blacklist
+# - hash value is a boolean to enable/disable this feature
+# @since 0.1.0
 default[:common_attrs][:blacklists] = {}
 
